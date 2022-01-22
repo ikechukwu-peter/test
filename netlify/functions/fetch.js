1200454a3f2b97e4b3b4bf739f556109f2894723
ikechukwu-peter <pitiyke@gmail.com>
2022-01-22 03:54:28 +0100

    m

--- netlify/functions/fetch.js
+++ netlify/functions/fetch.js
@@ -1,13 +1,13 @@
-const fetch = require("node-fetch");
-let dotenv = require("dotenv")
-dotenv.config({ path: './.env' });
+import fetch from "node-fetch";
+import { config } from "dotenv";
+config({ path: './.env' });
 
 
 // const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
 
 
 
-exports.handler =  (event, context) => {
+export function handler(event, context) {
     console.log(event);
     console.log(context);
  return {
@@ -15,5 +15,5 @@ exports.handler =  (event, context) => {
      key: process.env.KEY,
      url: process.env.URL
  }
-};
+}
           
